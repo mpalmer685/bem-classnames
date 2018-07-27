3eca056bd4a6299b9f6406106f586bd5dd414f3c
Mike Palmer <mrpalmer685@gmail.com>
2018-07-27 16:58:12 -0400

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module'
+    },
+
+    extends: 'eslint:recommended',
+
+    rules: {
+        curly: 'error'
+    }
+}
